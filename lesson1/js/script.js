01132b38e938c3af34b55ade9ea871cb78236c5e
Maxim <maxklimov13@yandex.ru>
2018-08-07 15:09:07 +0300

    Первый комит

---
+++ lesson1/js/script.js
@@ -0,0 +1,16 @@
+let budget = +prompt("Ваш бюджет?");
+let shopName = prompt("Название вашего магазина?");
+let shopGoods = [];
+let employers = {};
+let open;
+let mainList = {
+	budget,
+	shopName,
+	shopGoods,
+	employers,
+	open
+}
+shopGoods.push(prompt("Какой тип товаров будем продавать?"));
+shopGoods.push(prompt("Какой тип товаров будем продавать?"));
+shopGoods.push(prompt("Какой тип товаров будем продавать?"));
+let budget1day = alert( "Бюджет за 1 день равен: " + budget/30 );
